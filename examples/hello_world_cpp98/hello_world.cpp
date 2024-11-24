21bd2ec942eafb0a3ca8128745e0d2e66de1ee10
Gammasoft <gammasoft71@gmail.com>
2024-11-24 19:40:27 +0100

    Update hello_world.cpp

--- examples/hello_world_cpp98/hello_world.cpp
+++ examples/hello_world_cpp98/hello_world.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
 int main() {
+  if (auto i = 54)
+    std::cout << "i == 54" << std::endl;
+  else
+    std::cout << "i != 54" << std::endl;
+
   std::cout << "Hello, World!" << std::endl;
 }
